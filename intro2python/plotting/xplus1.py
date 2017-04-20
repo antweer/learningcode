d70681aa212d936f3b992eb08542ca396fef0bb3
antweer <trajwani@gmail.com>
2017-04-20 10:27:51 -0500

    I plotted f(x)=x+1 for x values of -3 to 3 in increments of 1

---
+++ intro2python/plotting/xplus1.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env Python3
+
+from matplotlib import pyplot
+
+def f(x):
+	return x+1
+
+xs = list(range(-3,4))
+ys = []
+for x in xs:
+	ys.append(f(x))
+
+pyplot.plot(xs, ys)
+pyplot.show()
