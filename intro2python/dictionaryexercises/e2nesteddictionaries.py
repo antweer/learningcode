48c05b54c66528dbb3dbdcabc7050c02e9a7a894
antweer <trajwani@gmail.com>
2017-04-24 11:01:45 -0500

    This exercise dealth with dictionaries nested with lists and dictionaries.

---
+++ intro2python/dictionaryexercises/e2nesteddictionaries.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+ramit = {
+    'name': 'Ramit',
+    'email': 'ramit@gmail.com',
+    'interests': ['movies', 'tennis'],
+    'friends': [
+        {
+            'name': 'Jasmine',
+            'email': 'jasmine@yahoo.com',
+            'interests': ['photography', 'tennis']
+        },
+        {
+            'name': 'Jan',
+            'email': 'jan@hotmail.com',
+            'interests': ['movies', 'tv']
+        }
+    ]
+}
+
+if __name__ == "__main__":
+    print(ramit["email"])
+    print(ramit["interests"][0])
+    print(ramit["friends"][0]["email"])
+    print(ramit["friends"][1]["interests"][1])
+
+    
