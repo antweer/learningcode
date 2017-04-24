5e5775c1d651d002fade76f01e90ebfe9c29aed0
antweer <trajwani@gmail.com>
2017-04-24 11:49:54 -0500

    This script takes a string as an input then prints our a dictionary that tallies the amount of times each word appears in that string

---
+++ intro2python/dictionaryexercises/word_summary.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+import string
+
+def word_histogram(text):
+    tally = {}
+    words = text.split()
+    for x in words:
+        tally[x] = text.count(x)
+    print(tally)
+
+if __name__ == "__main__":
+    paragraph = input("Statement: ").lower()
+    word_histogram(paragraph)
+
+    
+
