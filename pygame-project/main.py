ff6aeb4b63aad0e2afec2aabfdf78029811a85a4
antweer <trajwani@gmail.com>
2017-04-28 09:40:09 -0500

    Fixed goblin added

--- pygame-project/main.py
+++ pygame-project/main.py
@@ -3,108 +3,10 @@
 import pygame
 from random import randint
 from math import sqrt, fabs
-
-class Character():
-    def __init__(self, xcoord, ycoord):
-        self.xcoord = xcoord
-        self.ycoord = ycoord
-    def movenorth(self, screen):
-        if self.ycoord > 20:
-            self.ycoord -=15
-        else:
-            self.ycoord = 450
-        screen.blit(self.image, (self.xcoord,self.ycoord))
-    def movesouth(self, screen):
-        if self.ycoord < 460:
-            self.ycoord +=15
-        else:
-            self.ycoord = 15
-        screen.blit(self.image, (self.xcoord,self.ycoord))
-    def moveeast(self, screen):
-        if self.xcoord <500:
-            self.xcoord +=15
-        else:
-            self.xcoord = 15
-        screen.blit(self.image, (self.xcoord,self.ycoord))
-    def movewest(self, screen):
-        if self.xcoord > 20:
-            self.xcoord -=15
-        else:
-            self.xcoord = 490
-        screen.blit(self.image, (self.xcoord,self.ycoord))
-    def movenorthwest(self, screen):
-        self.movenorth(screen)
-        self.movewest(screen)
-    def movesouthwest(self, screen):
-        self.movesouth(screen)
-        self.movewest(screen)
-    def movenortheast(self, screen):
-        self.movenorth(screen)
-        self.moveeast(screen)
-    def movesoutheast(self, screen):
-        self.movesouth(screen)
-        self.moveeast(screen)
-    def stay(self, screen):
-        screen.blit(self.image, (self.xcoord,self.ycoord))
-
-class Hero(Character):
-    def __init__(self, xcoord = 243, ycoord = 228):
-        self.xcoord = xcoord
-        self.ycoord = ycoord
-        self.image = pygame.image.load("images/hero.png")
-        self.limiter = 10
-    def move(self, screen, framecount):
-        if framecount%self.limiter == 0:
-            if self.ycoord > 15 and pygame.key.get_pressed()[pygame.K_UP]:
-                self.movenorth(screen)
-            elif self.ycoord < 450 and pygame.key.get_pressed()[pygame.K_DOWN]:
-                self.movesouth(screen)
-            elif self.xcoord > 15 and pygame.key.get_pressed()[pygame.K_LEFT]:
-                self.movewest(screen)
-            elif self.xcoord < 490 and pygame.key.get_pressed()[pygame.K_RIGHT]:
-                self.moveeast(screen)  
-            else:
-                self.stay(screen) 
-        else:
-            self.stay(screen)
-
-class Enemy(Character):
-     def __init__(self, xcoord = randint(20,480) , ycoord = randint(20,460)):
-        self.xcoord = xcoord
-        self.ycoord = ycoord
-        self.limiter = 60
-     def movemonster(self, screen, framecount):
-        moves = [self.movenorth, 
-                self.movesouth, 
-                self.moveeast, 
-                self.movewest, 
-                self.movenortheast, 
-                self.movenorthwest,
-                self.movesouthwest,
-                self.movesoutheast
-                ]
-        randomdir = randint(0,7)
-        if framecount%self.limiter == 0:
-            return moves[randomdir](screen)
-        else:
-            return self.stay(screen)    
-
-class Monster(Enemy):
-    def __init__(self, xcoord = randint(20,480) , ycoord = randint(20,460)):
-        self.xcoord = xcoord
-        self.ycoord = ycoord
-        self.image = pygame.image.load("images/monster.png")
-        self.limiter = 15
-
-class Goblin(Enemy):
-    def __init__(self, xcoord = randint(20,480) , ycoord = randint(20,460)):
-        self.xcoord = xcoord
-        self.ycoord = ycoord
-        self.image = pygame.image.load("images/goblin.png")
-        self.limiter = 20
-
+from characters import *
     
 def main():
+    #Game initialization
     width = 510
     height = 480
     black_color = (0, 0, 0)
@@ -121,27 +23,22 @@ def main():
     font = pygame.font.Font(None, 40)
     wintext = font.render('Hit ENTER to play again!', True, black_color)
     losetext = font.render('You lose! Hit ENTER to play again', True, black_color)
-    
-    # Game initialization
     level = 0
-    monster = Monster()
     hero = Hero()
-    goblin1 = Goblin()
-    goblin2 = Goblin()
-    goblin3 = Goblin()
-    enemies = {1:monster, 2:goblin1, 3:goblin2, 4:goblin3}
-    goblins = {1:goblin1, 2:goblin2, 3:goblin3}
+    goblins = {1:Goblin(), 2:Goblin(), 3:Goblin()}
+    enemies = {1:Monster(), 2:goblins[1], 3:goblins[2], 4:goblins[3]}
     framecount = 0
     stop_game = False
     playing = True
     winorloss = ""
+
     while not stop_game:
         leveltext = font.render("Level {}".format(level), True, black_color)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 stop_game = True
         if level > 0:      
-            for x in range(0, level):
+            for x in range(0, level+1):
                 goblins[x+3] = Goblin()
                 enemies[x+4] = Goblin()
         else:
@@ -170,7 +67,7 @@ def main():
         screen.blit(background, (0, 0))
         hero.stay(screen)   
 
-        if winorloss == monster:
+        if winorloss == enemies[1]:
             winsound.play()
             screen.blit(wintext, (75, 230))
         else:
@@ -180,7 +77,7 @@ def main():
         
         pygame.display.update()
 
-        if winorloss == monster and pygame.key.get_pressed()[pygame.K_RETURN]:
+        if winorloss == enemies[1] and pygame.key.get_pressed()[pygame.K_RETURN]:
             level += 1
 
         if pygame.key.get_pressed()[pygame.K_RETURN]:
