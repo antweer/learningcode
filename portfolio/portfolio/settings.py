338fe58bbe8f52c7466f9b1f3c23d75944aea117
antweer <trajwani@gmail.com>
2017-05-09 12:13:52 -0500

    updated allowed host

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -25,7 +25,9 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'dev')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '104.236.213.240',
+]
 
 
 # Application definition
