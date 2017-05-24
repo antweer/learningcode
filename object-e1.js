efcec2065648bf17df376eaf31c28845d7cbd848
antweer <trajwani@gmail.com>
2017-05-24 12:06:54 -0500

    Made object classes for creating a  deck, hand, and cards

--- object-e1.js
+++ object-e1.js
@@ -59,3 +59,89 @@ class Hand {
 }
 
 // Deck constructor
+class Deck {
+  constructor(){
+    this.cards = [
+    { point: 1, suit: 'hearts' },
+    { point: 1, suit: 'spades' },
+    { point: 1, suit: 'diamonds' },
+    { point: 1, suit: 'clubs' },
+    { point: 2, suit: 'hearts' },
+    { point: 2, suit: 'spades' },
+    { point: 2, suit: 'diamonds' },
+    { point: 2, suit: 'clubs' },
+    { point: 3, suit: 'hearts' },
+    { point: 3, suit: 'spades' },
+    { point: 3, suit: 'diamonds' },
+    { point: 3, suit: 'clubs' },
+    { point: 4, suit: 'hearts' },
+    { point: 4, suit: 'spades' },
+    { point: 4, suit: 'diamonds' },
+    { point: 4, suit: 'clubs' },
+    { point: 5, suit: 'hearts' },
+    { point: 5, suit: 'spades' },
+    { point: 5, suit: 'diamonds' },
+    { point: 5, suit: 'clubs' },
+    { point: 6, suit: 'hearts' },
+    { point: 6, suit: 'spades' },
+    { point: 6, suit: 'diamonds' },
+    { point: 6, suit: 'clubs' },
+    { point: 7, suit: 'hearts' },
+    { point: 7, suit: 'spades' },
+    { point: 7, suit: 'diamonds' },
+    { point: 7, suit: 'clubs' },
+    { point: 8, suit: 'hearts' },
+    { point: 8, suit: 'spades' },
+    { point: 8, suit: 'diamonds' },
+    { point: 8, suit: 'clubs' },
+    { point: 9, suit: 'hearts' },
+    { point: 9, suit: 'spades' },
+    { point: 9, suit: 'diamonds' },
+    { point: 9, suit: 'clubs' },
+    { point: 10, suit: 'hearts' },
+    { point: 10, suit: 'spades' },
+    { point: 10, suit: 'diamonds' },
+    { point: 10, suit: 'clubs' },
+    { point: 11, suit: 'hearts' },
+    { point: 11, suit: 'spades' },
+    { point: 11, suit: 'diamonds' },
+    { point: 11, suit: 'clubs' },
+    { point: 12, suit: 'hearts' },
+    { point: 12, suit: 'spades' },
+    { point: 12, suit: 'diamonds' },
+    { point: 12, suit: 'clubs' },
+    { point: 13, suit: 'hearts' },
+    { point: 13, suit: 'spades' },
+    { point: 13, suit: 'diamonds' },
+    { point: 13, suit: 'clubs' },
+  ];
+    this.deck = [];
+  }
+  shuffle () {
+    let cardsPile = this.cards.length;
+    let deckPile = this.deck.length;
+    if(cardsPile > 0){
+      for(let x=0; x<cardsPile; x++){
+        this.deck.push(this.cards.splice(random(0,this.cards.length),1)[0]);
+      }
+    }else{
+      for(let x=0; x<deckPile; x++){
+        this.cards.push(this.deck.splice(random(0,this.deck.length),1)[0]);
+      }
+    }
+  }
+  draw() {
+    if(this.deck.length > 0){
+      return(this.deck.pop());
+    }else{
+      return(this.cards.pop());
+    }
+  }
+  numCardsLeft(){
+    if(this.deck.length > 0){
+      return(this.deck.length);
+    }else{
+      return(this.cards.length);
+    }
+  }
+}
