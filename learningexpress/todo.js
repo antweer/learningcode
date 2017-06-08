49508908f1ebc0b264ee2cd9e5cd0df04ade913a
antweer <trajwani@gmail.com>
2017-06-08 10:16:20 -0500

    added login on the todo app

--- learningexpress/todo.js
+++ learningexpress/todo.js
@@ -1,17 +1,36 @@
 var express = require('express');
 var app = express();
 var body_parser = require('body-parser');
-
+var session = require('express-session');
 var promise = require('bluebird');
 var pgp = require('pg-promise')({
   promiseLib: promise
 });
 var db = pgp({database: 'test'});
 
+var morgan = require('morgan');
+app.use(morgan('dev'));
+
 app.set('view engine', 'hbs');
 
 app.use(body_parser.urlencoded({extended: false}));
 app.use('/static', express.static('public'));
+app.use(session({
+  secret: process.env.SECRET_KEY || 'dev', 
+  resave: true,
+  saveUninitialized: false,
+  cookie: {maxAge: 600000}
+}));
+
+app.use(function(request, response, next){
+  if (request.session.user) {
+    next();
+  }else if (request.path == '/login'){
+    next();
+  } else {
+    response.redirect('/login');
+  }
+});
 
 app.get('/todos', function(request, response, next){
   let query = "SELECT * FROM task";
@@ -44,6 +63,20 @@ app.post('/submit', function(request, response){
     });
 });
 
+app.get('/login', function(request, response){
+  response.render('login.hbs');
+});
+
+app.post('/login', function (request, response) {
+  var username = request.body.username;
+  var password = request.body.password;
+  if (username == 'tanweer' && password == 'tanweer') {
+    request.session.user = username;
+    response.redirect('/todos');
+  } else {
+    response.render('login.hbs');
+  }
+});
 
 app.listen(8000, function(){
   console.log('Listening on port 8000')
