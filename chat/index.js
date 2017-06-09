befc309518afda5dce0d3af4506607c9c4fd3e02
antweer <trajwani@gmail.com>
2017-06-09 14:55:49 -0500

    Learned and practiced socket.io by making my own chat app

---
+++ chat/index.js
@@ -0,0 +1,23 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.set('view engine', 'hbs');
+
+app.get('/', function(req, res){
+  res.render('chat2.hbs');
+});
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+  socket.on('chat messages', function(msg){
+    io.emit('chat messages', msg);
+  });
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+});
+
+http.listen(3000, function(){
+  console.log('listening on 3000');
+});
