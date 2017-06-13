31a5ace4df2bf201aebd3dbb869379d78a374e85
antweer <trajwani@gmail.com>
2017-06-13 10:37:57 -0500

    deploying chat app

--- learningexpress/fabfile.py
+++ learningexpress/fabfile.py
@@ -3,14 +3,14 @@ from fabric.api import run, env, sudo, cd, prefix, shell_env
 env.hosts = ['104.236.213.240']
 env.user = 'tanweer'
 
-DIR = '/home/tanweer/learningjs/learningexpress'
+DIR = '/home/tanweer/learningjs/chat'
 VENV = 'echo Hello'
 
 def start ():
   with cd(DIR):
     with shell_env(PATH='/home/tanweer/.nvm/versions/node/v6.10.3/bin:$PATH'):
       with prefix(VENV):
-        run('pm2 start todo.js > start.log')
+        run('pm2 start app.js > start.log')
         
 def stop ():
   run('pm2 stop all > stop.log')
